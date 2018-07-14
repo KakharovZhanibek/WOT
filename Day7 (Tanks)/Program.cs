b3bc2454ec108abc4f86174d225cc8ed604ac27b
z.kontra <z.kontra@mail.ru>
2018-07-14 20:30:43 +0600

    Добавьте файлы проекта.

---
+++ Day7 (Tanks)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib.Module;
+
+namespace Day7__Tanks_
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Tank[] T34=new Tank[5];
+            Tank[] Panther=new Tank[5];
+            Tank[] WINNERS = new Tank[5];
+            for (int i = 0; i < 5; i++)
+            {
+                T34[i] = new Tank("T-34", rnd.Next(1, 100), rnd.Next(1, 100), rnd.Next(1, 100));
+                Panther[i] = new Tank("Panther", rnd.Next(1, 100), rnd.Next(1, 100), rnd.Next(1, 100));
+            }
+          
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Round: {0}\n--------", i + 1);
+                T34[i].PrintInfo();
+                Console.WriteLine("------------------");
+                Panther[i].PrintInfo();
+                WINNERS[i] = T34[i] * Panther[i];
+                Console.WriteLine("\n");
+                Console.WriteLine("---------------------------------------");
+                Console.Write("| Победитель в {0} раунде танк {1}", i + 1, WINNERS[i].GetTanksName());
+                Console.WriteLine("  |");
+                Console.WriteLine("---------------------------------------\n\n\n");
+            }
+        }
+    }
+}
---
+++ MyClassLib/WorldOfTanks/Tank.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib.Module
+{
+
+
+    public class Tank
+    {
+
+        private string Name { get; set; }
+        private int Ammunition { get; set; }
+        private int Armor { get; set; }
+        private int Maneuverability { get; set; }
+
+        public Tank()
+        {
+            Name = "None";
+            Ammunition = 0;
+            Armor = 0;
+            Maneuverability = 0;
+        }
+        public Tank(string Name, int Ammunition, int Armor, int Maneuverability)
+        {
+            this.Name = Name;
+            this.Ammunition = Ammunition;
+            this.Armor = Armor;
+            this.Maneuverability = Maneuverability;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Имя: {0}", Name);
+            Console.WriteLine("Боекомплект: {0}", Ammunition);
+            Console.WriteLine("Броня: {0}", Armor);
+            Console.WriteLine("Маневренность: {0}", Maneuverability);
+        }
+        public static Tank operator *(Tank T34, Tank Panther)
+        {
+            int t1 = 0, t2 = 0;
+            if (T34.Ammunition > Panther.Ammunition)
+                t1++;
+            else if (T34.Ammunition < Panther.Ammunition)
+                t2++;
+            else { }
+
+            if (T34.Armor > Panther.Armor)
+                t1++;
+            else if (T34.Armor < Panther.Armor)
+                t2++;
+            else { }
+            if (T34.Maneuverability > Panther.Maneuverability)
+                t1++;
+            else if (T34.Maneuverability < Panther.Maneuverability)
+                t2++;
+            else { }
+            if (t1 > t2)
+            {
+                return T34;
+            }
+            else if (t1 < t2)
+            {
+                return Panther;
+            }
+            else
+            {
+                int sum1 = 0, sum2 = 0;
+                sum1 = T34.Ammunition + T34.Armor + T34.Maneuverability;
+                sum2 = Panther.Ammunition + Panther.Armor + Panther.Maneuverability;
+                if (sum1 > sum2)
+                {
+                    return T34;
+                }
+                else
+                {
+                    return Panther;
+                }
+            }
+        }
+        public string GetTanksName()
+        {
+            return Name;
+        }
+    }
+}
